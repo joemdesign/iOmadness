8a4de6047d49e3de520791d87bba32b50d6e89d7
Joe Morris <joem@joe-morriss-mbp.gateway.pace.com>
2014-05-10 19:23:13 -0400

    Initial Commit

---
+++ ifElseLoops/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ifElseLoops
+//
+//  Created by Joe Morris on 5/10/14.
+//  Copyright (c) 2014 joemdesign, llc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
